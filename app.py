879a21f7bbe5ddc40b32fef4546d4959a7cd8dee
Mohaman <abbio2004@gmail.com>
2018-07-28 18:31:17 -0700

    appJar and dat parsing

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
+import appJar
 from appJar import gui
 
+APTDICT = {}
+
 def loadData():
-    x = open("airports.dat", "r")
-    for line in x:
-        print(line)
+    x = open("airports.dat", "r", encoding="utf8")
+    for lines in x:
+        tempDict = {}
+        g = lines.split(',')
+        
+        name = g[1].strip('"')
+        city = g[2].strip('"')
+        country = g[3].strip('"')
+        icaoCode = g[5].strip('"')
+        tempDict = {"Name" : name, "City" : city, "Country" : country, "ICAO" : icaoCode}
+        
+        APTDICT[name] = tempDict
+    x.close()
 
 def main():
     loadData()
+    #print(APTDICT["Tucson International Airport"])
+
+    for (k, v) in APTDICT.items():
+        if v['City'] == "Tucson":
+            print(k)
+
     app = gui()
     app.addLabel("My Test Project")
-    app.go()
+    #app.go()
 
 main()
